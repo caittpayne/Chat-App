56acc5632b6cf41583def93794cae6530dd38340
Cait Payne <caitpayne@outlook.com>
2018-08-10 10:02:03 -0600

    Completed room list checkpoint

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,23 +1,39 @@
 import React, { Component } from 'react';
 
 class RoomList extends Component {
+  constructor(props) {
+    super(props);
 
-  this.state = {
-    rooms: []
-  };
+    this.state = {
+      rooms: []
+    };
 
-  this.roomsRef = this.props.firebase.database().ref('rooms');
+    this.roomsRef = this.props.firebase.database().ref('rooms');
+  }
 
   componentDidMount() {
     this.roomsRef.on('child_added', snapshot => {
-      console.log(snapshot);
+      const room = snapshot.val();
+      room.key = snapshot.key;
+      this.setState({ rooms: this.state.rooms.concat( room )})
     });
   }
 
-  render {
-    return (
 
-    );
+
+  render() {
+    return (
+      <section className='roomList'>
+      {
+        this.state.rooms.map((room, index) =>
+          <div key={index}>{room.name}</div>
+        )
+      }
+      </section>
+    )
+
+
+
   }
 }
 
